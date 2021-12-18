bc84a5b8e9499f1f613430b655c226d244edde4e
Halston <sellenth@oregonstate.edu>
2021-12-17 16:36:41 -0800

    bit shifting was overflowing. make sure to cast left hand of shift operator. Pain in the butt to debug that

--- day16/main.cpp
+++ day16/main.cpp
@@ -14,11 +14,11 @@ char FILENAME[] = "./ex.txt";
 char FILENAME[] = "./input.txt";
 #endif
 
-#define F(i, a, b) for(int i = a; i < b; i++)
+#define F(i, a, b) for(long long i = a; i < b; i++)
 
 struct Packet {
-    int version;
-    int type;
+    long long version;
+    long long type;
     vector<string> words;
     vector<Packet> packets;
 };
@@ -65,36 +65,37 @@ public:
         return c - 48 == 1;
     }
     
-    int binaryStringToInt(string &orig_str, int &i, int inc){
+    long long binaryStringToLL(string &orig_str, long long &i, long long inc){
         string s = orig_str.substr(i, inc);
         i+= inc;
-        int res = 0;
-        int power = s.size() - 1;
-        for(int i = 0; i < s.size(); i++){
+
+        long long res = 0;
+        long long power = s.size() - 1;
+        for(long long i = 0; i < s.size(); i++){
             if (isOne(s[i])){
-                res += 1 << power;
+                res += (long long)1 << power;
             }
             power--;
         }
         return res;
     }
 
-    int pWordsToInt(vector<string> &words){
+    long long pWordsToLL(vector<string> &words){
         string wordsCat = "";
-        for(int i = 0; i < words.size(); i++){
+        for(long long i = 0; i < words.size(); i++){
             wordsCat.append(words[i]);
         }
-        int etc = 0;
-        return binaryStringToInt(wordsCat, etc, wordsCat.size());
+        long long etc = 0;
+        return binaryStringToLL(wordsCat, etc, wordsCat.size());
     }
 
-    Packet parsePacket(string s, int &i, bool align = true){
+    Packet parsePacket(string s, long long &i, bool align = true){
         Packet p;
-        p.version = binaryStringToInt(s, i, 3);
-        p.type = binaryStringToInt(s, i, 3);
+        p.version = binaryStringToLL(s, i, 3);
+        p.type = binaryStringToLL(s, i, 3);
         if (p.type == 4){ // This is a literal Packet
             while (i < s.size()){
-                int keepGoing = binaryStringToInt(s, i, 1);
+                long long keepGoing = binaryStringToLL(s, i, 1);
                 if (keepGoing){
                     p.words.push_back(s.substr(i, 4));
                     i+=4;
@@ -105,16 +106,16 @@ public:
                 }
             }
         } else { //This is an operator packet
-            int pOp = binaryStringToInt(s, i, 1); 
+            long long pOp = binaryStringToLL(s, i, 1); 
             if (pOp == 0){ // totalLength
-                int length = binaryStringToInt(s, i, 15);
-                int stopIdx = i + length;
+                long long length = binaryStringToLL(s, i, 15);
+                long long stopIdx = i + length;
 
                 while (i < stopIdx){
                     p.packets.push_back(parsePacket(s, i, false));
                 }
             } else { // Number of following packets
-                int numSubPackets = binaryStringToInt(s, i, 11);
+                long long numSubPackets = binaryStringToLL(s, i, 11);
                 while (numSubPackets > 0){
                     numSubPackets--;
                     p.packets.push_back(parsePacket(s, i, false));
@@ -132,12 +133,12 @@ public:
     }
 
     void tests(){
-        int i = 0;
+        long long i = 0;
         Packet L = parsePacket("110100101111111000101000", i);
         cout << i << endl;
         assert(L.version == 6);
         assert(L.type == 4);
-        assert(pWordsToInt(L.words) == 2021);
+        assert(pWordsToLL(L.words) == 2021);
         
         i = 0;
         Packet twoPack = parsePacket("00111000000000000110111101000101001010010001001000000000", i);
@@ -145,8 +146,8 @@ public:
 
         assert(twoPack.version == 1);
         assert(twoPack.type == 6);
-        assert(pWordsToInt(twoPack.packets[0].words) == 10);
-        assert(pWordsToInt(twoPack.packets[1].words) == 20);
+        assert(pWordsToLL(twoPack.packets[0].words) == 10);
+        assert(pWordsToLL(twoPack.packets[1].words) == 20);
 
         i = 0;
         Packet threePack = parsePacket("11101110000000001101010000001100100000100011000001100000", i);
@@ -154,13 +155,13 @@ public:
 
         assert(threePack.version == 7);
         assert(threePack.type == 3);
-        assert(pWordsToInt(threePack.packets[0].words) == 1);
-        assert(pWordsToInt(threePack.packets[1].words) == 2);
-        assert(pWordsToInt(threePack.packets[2].words) == 3);
+        assert(pWordsToLL(threePack.packets[0].words) == 1);
+        assert(pWordsToLL(threePack.packets[1].words) == 2);
+        assert(pWordsToLL(threePack.packets[2].words) == 3);
     }
 
-    int sumVersions(Packet p){
-        int sum = 0;
+    long long sumVersions(Packet p){
+        long long sum = 0;
         sum += p.version;
 
         for (auto &subPacket : p.packets){
@@ -170,16 +171,87 @@ public:
     }
 
     void solvePart1(){
-        int i = 0;
+        long long i = 0;
         Packet p = parsePacket(input, i);
 
-        cout << sumVersions(p) << endl;
+        cout << "The sum of all the versions is: " << sumVersions(p) << endl;
+    }
+
+    long long computePacket(Packet p){
+        long long ret = 0;
+        switch(p.type){
+            case 0: //sum
+                for (auto &subPacket : p.packets){
+                    ret += computePacket(subPacket);
+                }
+                break;
+            case 1: //product
+                {
+                    long long product = computePacket(p.packets[0]);
+                    for (long long i = 1; i < p.packets.size(); i++){
+                        product *= computePacket(p.packets[i]);
+                    }
+                    ret += product;
+                }
+                break;
+            case 2: //minimum
+                {
+                long long min = -1;
+                for (auto &subPacket : p.packets){
+                    long long pValue = computePacket(subPacket);
+                    if (min == -1 || pValue < min){
+                        min = pValue;
+                    }
+                }
+                ret += min;
+                }
+                break;
+            case 3: //maximum
+                {
+                long long max = -1;
+                for (auto &subPacket : p.packets){
+                    long long pValue = computePacket(subPacket);
+                    if (max == -1 || pValue > max){
+                        max = pValue;
+                    }
+                }
+                ret += max;
+                }
+                break;
+            case 4: //literal
+                ret += pWordsToLL(p.words);
+                break;
+            case 5: //greater than
+                if (computePacket(p.packets[0]) > computePacket(p.packets[1])){
+                    ret += 1;
+                }
+                break;
+            case 6: //less than
+                if (computePacket(p.packets[0]) < computePacket(p.packets[1])){
+                    ret += 1;
+                }
+                break;
+            case 7: //less than
+                if (computePacket(p.packets[0]) == computePacket(p.packets[1])){
+                    ret += 1;
+                }
+                break;
+        }
+        return ret;
+    }
+
+    void solvePart2(){
+        long long i = 0;
+        Packet p = parsePacket(input, i);
+
+        cout << computePacket(p) << endl;
     }
 
     Solution() {
         parseInput();
-        //tests();
+        tests();
         solvePart1();
+        solvePart2();
     }
 };
 
